8e5ede157be9bda687f5e5ed3ca5ffdffc2ddeca
Sujal Singh <sujalsingh1920@gmail.com>
2026-02-03 15:59:26 +0530

    editing files just give output

--- scripts/cpu_monitor.sh
+++ scripts/cpu_monitor.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-source config/thresholds.conf
 
 CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')
 
 CPU_INT=${CPU%.*}
-
-echo "$(date) | CPU Usage: $CPU_INT%" >> logs/system.log
-
-if [ "$CPU_INT" -gt "$CPU_LIMIT" ]; then
-  echo "$(date) | CPU ALERT: $CPU_INT%" >> logs/alerts.log"
-fi
 
