f1f0adfab4fde88038291393e6ff5a6bee32b940
Sujal Singh <sujalsingh1920@gmail.com>
2026-03-02 23:45:07 +0530

    automate the main.sh file using crontab and also use logrotate upto 3 rotation for backup

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,30 +1,52 @@
+
 #!/bin/bash
+
+LOG_FILE="../logs/system.log"
+
 source ../config/threshold.sh
- 
+
 CPU_VALUE=$(./cpu_monitor.sh)
 DISK_VALUE=$(./disk_monitor.sh)
 MEMORY_VALUE=$(./memory_monitor.sh)
 
-if [ "$CPU_VALUE" -gt "$CPU_LIMIT" ]; 
+CURRENT_TIME=$(date '+%Y-%m-%d %H:%M:%S')
+
+if [ "$CPU_VALUE" -gt "$CPU_LIMIT" ];
 then
-	echo "ERROR : $CPU_VALUE"
+    CPU_STATUS="ERROR"
 else
-	echo " CPU IS OKAYY : '$CPU_VALUE'"
+    CPU_STATUS="OK"
 fi
+
 
 if [ "$DISK_VALUE" -gt "$DISK_LIMIT" ];
 then
-        echo "ERROR : $CPU_VALUE"
+    DISK_STATUS="ERROR"
 else
-        echo " DISK IS OKAYY : '$DISK_VALUE'"
-fi
- 
-if [ "$MEMORY_VALUE" -gt "$RAM_LIMIT" ];
-then
-        echo "ERROR : $CPU_VALUE"
-else
-        echo " RAM IS OKAYY : '$MEMORY_VALUE'"
+    DISK_STATUS="OK"
 fi
 
+
+if [ "$MEMORY_VALUE" -gt "$RAM_LIMIT" ]; 
+then
+    RAM_STATUS="ERROR"
+else
+    RAM_STATUS="OK"
+fi
+
+
+# -------- Terminal Output (Single Line) --------
+echo "$CURRENT_TIME | CPU:$CPU_VALUE ($CPU_STATUS) | DISK:$DISK_VALUE ($DISK_STATUS) | RAM:$MEMORY_VALUE ($RAM_STATUS)"
+
+
+# -------- Append Data Row --------
+{
+printf "%-20s | %-20s | %-20s | %-20s\n" \
+"$CPU_VALUE ($CPU_STATUS)" \
+"$DISK_VALUE ($DISK_STATUS)" \
+"$MEMORY_VALUE ($RAM_STATUS)" \
+"$CURRENT_TIME" >> "$LOG_FILE"
+
+} | tee -a "$LOG_FILE"
 
 
